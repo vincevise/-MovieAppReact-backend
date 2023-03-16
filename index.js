b88f5d14403d89234f59a94a6b86cfff5e967439
Vincent Vuram <vincentvuram204@gmail.com>
2023-03-16 11:05:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const jsonServer = require('json-server')
+const cors = require('cors')
+
+const server = jsonServer.create()
+const router = jsonServer.router( 'data.json')
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use('/api',(req,res)=>{
+  try{
+    res.status(200).json({data:'hogaya'})
+  }catch(error){
+    res.status(400).json(error.message)
+  }
+})
+server.use(router)
+
+const PORT = 3026
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
